f3be93f86df847212764fc53289710d8007f9ca3
Raymall Perez <raymall.perez@gmail.com>
2021-08-21 18:24:13 -0400

    added fake api response [wip]

--- app/src/scripts/views/index.js
+++ app/src/scripts/views/index.js
@@ -1,9 +1,22 @@
 import $ from 'jquery';
 import forEach from 'lodash/forEach';
+import { createServer } from "miragejs"
 require('parsleyjs');
 
 (function() {
+  createServer({
+    routes() {
+      this.post("/api/submission", () => {
+        return { 
+          "status": "success", 
+          "message": "Thank you. You are now subscribed." 
+         }
+      })
+    },
+  })
+
   let FORM_ERRORS = []
+
   let formInstance = $('#form').parsley({
     focus: 'none',
     trigger: false,
@@ -12,24 +25,8 @@ require('parsleyjs');
     errorClass: "error"
   })
 
-  formInstance.on('form:submit', () => {
-    alert('valid')
-    $('.js-form-errors')
-    .empty()
-    .addClass('visually-hidden')
-    .append(`<p>No errors</p>`)
-    return false;
-  })
-
   formInstance.on('field:error', (fieldInstance) => {
-    console.log(`<span>${ $(fieldInstance.$element).attr('name') } is required</span>`)
     FORM_ERRORS.push(`<span>${ $(fieldInstance.$element).attr('name').replace('[]', '') } is required</span>`)
-  })
-
-  formInstance.on('form:validate', (formInstance) => {
-    $('.js-form-errors').empty()
-    FORM_ERRORS = []
-    console.log('validate', formInstance)
   })
 
   formInstance.on('form:error', (formInstance) => {
@@ -40,9 +37,38 @@ require('parsleyjs');
       $('.js-form-errors')
       .removeClass('visually-hidden')
       .append(value)
-      console.log(value)
+    })
+  })
+
+  formInstance.on('form:validate', () => {
+    $('.js-form-errors').empty()
+    FORM_ERRORS = []
+  })
+
+  formInstance.on('form:submit', () => {
+    $('.js-form-errors')
+    .empty()
+    .addClass('visually-hidden')
+    .append(`<p>No errors</p>`)
+    
+    fetch('/api/submission/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: $('#form').serialize()
+    })
+    .then(response => {
+      if (!response.ok) throw Error(response.statusText)
+      return response.json()
+    })
+    .then((json) => {
+      // buildList(json)
+      console.log(json)
+    }).catch((errors) => {
+      console.log(errors)
     })
 
-    FORM_ERRORS = []
+    return false;
   })
 })();
