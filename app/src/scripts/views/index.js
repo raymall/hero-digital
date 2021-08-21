8dd1aef57a918a19a0e3bb18c8d441de05083e5e
Raymall Perez <raymall.perez@gmail.com>
2021-08-21 15:38:21 -0400

    added form validation [wip]

--- app/src/scripts/views/index.js
+++ app/src/scripts/views/index.js
@@ -1,5 +1,65 @@
-import exportedFn from './util.js';
+import $ from 'jquery';
+require('parsleyjs');
 
 (function() {
-  exportedFn();
+  // $('#form').on('submit', (e) => {
+  //   e.preventDefault()
+  //   console.log('here')
+  //   let formInstance = $('#form').parsley({
+  //     uiEnabled: false,
+  //     trigger: 'change',
+  //     focus: 'first',
+  //     successClass: "success",
+  //     errorClass: "error"
+  //   })
+  //   if (formInstance.isValid()){
+  //     alert('valid');
+  //   } else {
+  //     alert('is not valid');
+  //   }
+  // })
+
+  // $('#form').on('submit', (e) => {
+  //   console.log('here')
+  //   let formInstance = $('#form').parsley({
+  //     uiEnabled: false,
+  //     trigger: 'change',
+  //     focus: 'first',
+  //     successClass: "success",
+  //     errorClass: "error"
+  //   })
+  //   if (formInstance.isValid()){
+  //     alert('valid');
+  //   } else {
+  //     alert('is not valid');
+  //   }
+  //   e.preventDefault()
+  // })
+
+  $('#form').parsley({
+    successClass: "success",
+    errorClass: "error"
+  })
+  .on('form:submit', () => {
+    alert('valid');
+    return false; // Don't submit form for this demo
+  });
+
+  $.listen('parsley:field:error', (fieldInstance) => {
+    console.log(fieldInstance);
+  });
+
+  // $('#form').parsley({
+  //   uiEnabled: false,
+  //   focus: 'first',
+  //   successClass: "success",
+  //   errorClass: "error"
+  // }).on('form:validate', function() {
+  //   // var ok = $('.parsley-error').length === 0;
+  //   // $('.bs-callout-info').toggleClass('hidden', !ok);
+  //   // $('.bs-callout-warning').toggleClass('hidden', ok);
+  // })
+  // .on('form:submit', function() {
+  //   return false; // Don't submit form for this demo
+  // });
 })();
